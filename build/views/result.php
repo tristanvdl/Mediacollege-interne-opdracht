b76d836b3b1b3c85b5bac1995720c6b07d1e0d07
Tristan van der Linden <tristanvdl@gmail.com>
2016-10-10 11:08:22 +0200

    styled the ticket button

--- build/views/result.php
+++ build/views/result.php
@@ -5,8 +5,7 @@ $item = $db->SingleItem('procedures');
 $_keys = $db->PushTicketData();
 foreach ($item as $key) {
     ?>
-
-    <div class="container resultcontent">
+    <div class="container result-content">
         <div class="wrapper_results resultcontentchild">
             <div class="row">
                 <h1 class="pagetitle restitle"><?php echo $key['dienst'] ?></h1>
@@ -30,48 +29,39 @@ foreach ($item as $key) {
                 </div>
 
                 <div class="col-md-2"></div>
-
-                <div class="link col-md-5">
-                    <h2 class="columntitle">Link</h2>
-                    <?php echo $key['link'] ?>
+                
+                <div class="button_aanvragen col-md-5">
+                    <span class="result-button" data-toggle="modal" data-target="#myModal"><p>Ticket Aanvragen</p><i class="fa fa-2x fa-long-arrow-right result-arrow" aria-hidden="true"></i></span>
                 </div>
             </div>
-            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
         </div>
         <div id="myModal" class="modal fade" role="dialog">
             <div class="modal-dialog">
                 <!-- Modal content-->
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Maak een ticket aan</h4>
-                    </div>
-                    <div class="modal-body">
-                        <form action="?page=result&result=<?php echo $_GET['result']; ?>" method="post">
-                            onderwerp
-                            <input type="text" name="onderwerp"><br>
-                            beschrijving
-                            <input type="text" name="beschrijving"><br>
-                            Locatie
-                            <select name="locatie"><br>
-                                <option value="contactweg">Contactweg</option>
-                                <option value="dintelstraat">Dintelstraat</option>
-                            </select><br>
-                            Specifieke locatie
-                            <input type="text" name="specifieke_locatie"><br>
-                            spoed
-                            <select name="spoed"><br>
-                                <option value="0">Direct</option>
-                                <option value="1">Vandaag</option>
-                                <option value="2">Binnen een week</option>
-                                <option value="3">Geen spoed</option>
-                            </select><br>
-                            <input type="hidden" name="dienst" value="<?php echo $key['dienst'] ?>">
-                            <input type="submit">
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
+                    <h4 class="modal-title">Maak een ticket aan</h4>
+                    <form action="?page=result&result=<?php echo $_GET['result']; ?>" method="post">
+                        onderwerp
+                        <input type="text" name="onderwerp"><br>
+                        beschrijving
+                        <input type="text" name="beschrijving"><br>
+                        Locatie
+                        <select name="locatie"><br>
+                            <option value="contactweg">Contactweg</option>
+                            <option value="dintelstraat">Dintelstraat</option>
+                        </select><br>
+                        Specifieke locatie
+                        <input type="text" name="specifieke_locatie"><br>
+                        spoed
+                        <select name="spoed"><br>
+                            <option value="0">Direct</option>
+                            <option value="1">Vandaag</option>
+                            <option value="2">Binnen een week</option>
+                            <option value="3">Geen spoed</option>
+                        </select><br>
+                        <input type="hidden" name="dienst" value="<?php echo $key['dienst'] ?>">
+                        <input type="submit">
+                    </form>
                 </div>
             </div>
         </div>
