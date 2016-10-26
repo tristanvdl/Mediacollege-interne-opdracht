bc022f1221845ff681564d19df6d1bfe784fb7cf
Leroy Manshande <19392@ma-web.nl>
2016-10-26 16:32:07 +0200

    styled modal

--- build/views/result.php
+++ build/views/result.php
@@ -30,41 +30,38 @@ $keys = $pushData->PushTicketData();
                 </div>
 
                 <div class="col-md-2"></div>
-                
+
                 <div class="button_aanvragen col-md-5">
                     <span class="result-button" data-toggle="modal" data-target="#myModal"><p>Ticket Aanvragen</p><i class="fa fa-2x fa-long-arrow-right result-arrow" aria-hidden="true"></i></span>
                 </div>
             </div>
         </div>
+        <!-- create ticket starts here -->
         <div id="myModal" class="modal fade" role="dialog">
             <div class="modal-dialog">
                 <!-- Modal content-->
                 <div class="modal-content">
-                    <h4 class="modal-title">Maak een ticket aan</h4>
+                    <h4 class="modal-title">Melding indienen</h4>
                     <form action="?page=result&result=<?php echo $_GET['result']; ?>" method="post">
-                        onderwerp
-                        <input type="text" name="onderwerp"><br>
-                        beschrijving
-                        <input type="text" name="beschrijving"><br>
-                        Locatie
-                        <select name="locatie"><br>
+                        <input type="text" name="onderwerp" placeholder="onderwerp"><br>
+                        <input type="text" name="beschrijving" placeholder="beschrijving"><br>
+                        <select name="locatie" placeholder="locatie"><br>
                             <option value="contactweg">Contactweg</option>
                             <option value="dintelstraat">Dintelstraat</option>
                         </select><br>
-                        Specifieke locatie
-                        <input type="text" name="specifieke_locatie"><br>
-                        spoed
-                        <select name="spoed"><br>
-                            <option value="0">Direct</option>
-                            <option value="1">Vandaag</option>
-                            <option value="2">Binnen een week</option>
-                            <option value="3">Geen spoed</option>
+                        <input type="text" name="specifieke_locatie" placeholder="Specifieke locatie"><br>
+                        <select name="spoed" placeholder="spoed"><br>
+                            <option value="0" class="spoedoption">Direct</option>
+                            <option value="1" class="spoedoption">Vandaag</option>
+                            <option value="2" class="spoedoption">Binnen een week</option>
+                            <option value="3" class="spoedoption">Geen spoed</option>
                         </select><br>
                         <input type="hidden" name="dienst" value="<?php echo $item['dienst'] ?>">
-                        <input type="submit">
+                        <div class="button_aanvragen col-md-5">
+                            <span class="result-button createticket"><p>Vraag aan</p><i class="fa fa-2x fa-long-arrow-right result-arrow" aria-hidden="true"></i></span>
+                        </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-
